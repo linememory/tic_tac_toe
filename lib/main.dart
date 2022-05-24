f884ea103ec627f15443a8a1babdc7b29b10ec7c
Lukas Leitner <lukasleitner17@gmail.com>
2022-05-24 15:39:43 +0200

    changed app home to menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tic_tac_toe/features/game/views/tic_tac_toe.dart';
+import 'features/menu/menu.dart';
 
 final themeColorProvider = StateProvider<MaterialColor>((ref) {
   return Colors.amber;
@@ -16,13 +16,13 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final color = ref.watch(themeColorProvider);
-    
+
     return MaterialApp(
       title: 'Tic Tac Toe',
       theme: ThemeData(
         primarySwatch: color,
       ),
-      home: const TicTacToe(),
+      home: const Menu(),
     );
   }
 }
