c8ff9ccd773a278f84975d2d1e2ccf4fe6f28ac2
Lukas Leitner <lukasleitner17@gmail.com>
2022-05-25 14:50:48 +0200

    added theme and color mode

--- lib/features/menu/settings.dart
+++ lib/features/menu/settings.dart
@@ -7,7 +7,10 @@ class Settings extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
+    final themeMode = ref.watch(themeModeProvider);
+    final colorMode = ref.watch(colorModeProvider);
     return SimpleDialog(
+      insetPadding: const EdgeInsets.all(30),
       titlePadding: const EdgeInsets.all(10),
       contentPadding: const EdgeInsets.all(10),
       title: const Text(
@@ -15,77 +18,188 @@ class Settings extends ConsumerWidget {
         textAlign: TextAlign.center,
       ),
       children: [
-        Wrap(
-          children: [
-            _ColorButton(
-              color: Colors.purple,
-              onTab: (color) =>
-                  ref.read(themeColorProvider.state).state = color,
-            ),
-            _ColorButton(
-              color: Colors.pink,
-              onTab: (color) =>
-                  ref.read(themeColorProvider.state).state = color,
-            ),
-            _ColorButton(
-              color: Colors.red,
-              onTab: (color) =>
-                  ref.read(themeColorProvider.state).state = color,
-            ),
-            _ColorButton(
-              color: Colors.deepOrange,
-              onTab: (color) =>
-                  ref.read(themeColorProvider.state).state = color,
-            ),
-            _ColorButton(
-              color: Colors.amber,
-              onTab: (color) =>
-                  ref.read(themeColorProvider.state).state = color,
-            ),
-            _ColorButton(
-              color: Colors.yellow,
-              onTab: (color) =>
-                  ref.read(themeColorProvider.state).state = color,
-            ),
-            _ColorButton(
-              color: Colors.lime,
-              onTab: (color) =>
-                  ref.read(themeColorProvider.state).state = color,
-            ),
-            _ColorButton(
-              color: Colors.green,
-              onTab: (color) =>
-                  ref.read(themeColorProvider.state).state = color,
-            ),
-            _ColorButton(
-              color: Colors.indigo,
-              onTab: (color) =>
-                  ref.read(themeColorProvider.state).state = color,
-            ),
-            _ColorButton(
-              color: Colors.blue,
-              onTab: (color) =>
-                  ref.read(themeColorProvider.state).state = color,
-            ),
-            _ColorButton(
-              color: Colors.lightBlue,
-              onTab: (color) =>
-                  ref.read(themeColorProvider.state).state = color,
-            ),
-            _ColorButton(
-              color: Colors.teal,
-              onTab: (color) =>
-                  ref.read(themeColorProvider.state).state = color,
-            ),
-          ],
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Theme: "),
+                  DropdownButton<ThemeMode>(
+                    value: themeMode,
+                    items: const [
+                      DropdownMenuItem(
+                          value: ThemeMode.system, child: Text("System")),
+                      DropdownMenuItem(
+                          value: ThemeMode.light, child: Text("Light")),
+                      DropdownMenuItem(
+                          value: ThemeMode.dark, child: Text("Dark"))
+                    ],
+                    onChanged: (value) {
+                      if (value != null) {
+                        ref.read(themeModeProvider.state).state = value;
+                      }
+                    },
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("ColorMode"),
+                  DropdownButton<ColorMode>(
+                    value: colorMode,
+                    items: const [
+                      DropdownMenuItem(
+                          value: ColorMode.system, child: Text("System")),
+                      DropdownMenuItem(
+                          value: ColorMode.custom, child: Text("Custom"))
+                    ],
+                    onChanged: (value) {
+                      if (value != null) {
+                        ref.read(colorModeProvider.state).state = value;
+                      }
+                    },
+                  )
+                ],
+              ),
+              if (colorMode == ColorMode.custom)
+                Wrap(
+                  children: [
+                    _ColorButton(
+                      color: Colors.purple,
+                      onTab: (color) =>
+                          ref.read(themeColorProvider.state).state = color,
+                    ),
+                    _ColorButton(
+                      color: Colors.pink,
+                      onTab: (color) =>
+                          ref.read(themeColorProvider.state).state = color,
+                    ),
+                    _ColorButton(
+                      color: Colors.red,
+                      onTab: (color) =>
+                          ref.read(themeColorProvider.state).state = color,
+                    ),
+                    _ColorButton(
+                      color: Colors.deepOrange,
+                      onTab: (color) =>
+                          ref.read(themeColorProvider.state).state = color,
+                    ),
+                    _ColorButton(
+                      color: Colors.amber,
+                      onTab: (color) =>
+                          ref.read(themeColorProvider.state).state = color,
+                    ),
+                    _ColorButton(
+                      color: Colors.yellow,
+                      onTab: (color) =>
+                          ref.read(themeColorProvider.state).state = color,
+                    ),
+                    _ColorButton(
+                      color: Colors.lime,
+                      onTab: (color) =>
+                          ref.read(themeColorProvider.state).state = color,
+                    ),
+                    _ColorButton(
+                      color: Colors.green,
+                      onTab: (color) =>
+                          ref.read(themeColorProvider.state).state = color,
+                    ),
+                    _ColorButton(
+                      color: Colors.indigo,
+                      onTab: (color) =>
+                          ref.read(themeColorProvider.state).state = color,
+                    ),
+                    _ColorButton(
+                      color: Colors.blue,
+                      onTab: (color) =>
+                          ref.read(themeColorProvider.state).state = color,
+                    ),
+                    _ColorButton(
+                      color: Colors.lightBlue,
+                      onTab: (color) =>
+                          ref.read(themeColorProvider.state).state = color,
+                    ),
+                    _ColorButton(
+                      color: Colors.teal,
+                      onTab: (color) =>
+                          ref.read(themeColorProvider.state).state = color,
+                    ),
+                  ],
+                ),
+              const Divider(),
+              NameForm(onSubmit: (String player1, String player2) {}),
+            ],
+          ),
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 80.0),
-          child: ElevatedButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text("Save")),
-        )
       ],
+    );
+  }
+}
+
+class NameForm extends StatefulWidget {
+  const NameForm({
+    Key? key,
+    required this.onSubmit,
+  }) : super(key: key);
+
+  final void Function(String player1Name, String player2Name) onSubmit;
+
+  @override
+  State<NameForm> createState() => _NameFormState();
+}
+
+class _NameFormState extends State<NameForm> {
+  String player1 = "Player 1";
+  String player2 = "Player 2";
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            onChanged: (value) {
+              player1 = value;
+            },
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              isDense: true,
+              label: Text("Player 1 Name"),
+              contentPadding: EdgeInsets.all(10),
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          TextFormField(
+            onChanged: (value) {
+              player2 = value;
+            },
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              isDense: true,
+              label: Text("Player 2 Name"),
+              contentPadding: EdgeInsets.all(10),
+            ),
+          ),
+          Builder(builder: (context) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 80.0),
+              child: ElevatedButton(
+                  onPressed: () {
+                    if (Form.of(context)?.validate() ?? false) {
+                      widget.onSubmit(player1, player2);
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: const Text("Save")),
+            );
+          }),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,19 @@ final themeColorProvider = StateProvider<MaterialColor>((ref) {
   return Colors.amber;
 });
 
+final themeModeProvider = StateProvider<ThemeMode>((ref) {
+  return ThemeMode.system;
+});
+
+enum ColorMode {
+  system,
+  custom;
+}
+
+final colorModeProvider = StateProvider<ColorMode>((ref) {
+  return ColorMode.system;
+});
+
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
@@ -17,12 +30,16 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final color = ref.watch(themeColorProvider);
+    final themeMode = ref.watch(themeModeProvider);
+    final colorMode = ref.watch(colorModeProvider);
 
     return DynamicColorBuilder(
       builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
         ColorScheme lightColorScheme;
         ColorScheme darkColorScheme;
-        if (lightDynamic != null && darkDynamic != null) {
+        if (colorMode == ColorMode.system &&
+            lightDynamic != null &&
+            darkDynamic != null) {
           lightColorScheme = lightDynamic.harmonized();
           darkColorScheme = darkDynamic.harmonized();
         } else {
@@ -36,6 +53,7 @@ class MyApp extends ConsumerWidget {
         }
         return MaterialApp(
           title: 'Tic Tac Toe',
+          themeMode: themeMode,
           theme: ThemeData(colorScheme: lightColorScheme),
           darkTheme: ThemeData(colorScheme: darkColorScheme),
           home: const Menu(),
